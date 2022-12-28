d447c812c0b6185e92c7c06ffec24dfc6b4e91ff
Benjamin Reese <108361147+BenjaminFReese@users.noreply.github.com>
2022-12-28 15:12:07 -0500

    minor edits and pushing html file

---
+++ word_clouds.R
@@ -0,0 +1,48 @@
+## Word Clouds
+
+ctw <- words_cloud(CT)
+
+dew <- words_cloud(DE)
+
+gaw <- words_cloud(GA)
+
+maw <- words_cloud(MA)
+
+mdw <- words_cloud(MD)
+
+ncw <- words_cloud(NC)
+
+nhw <- words_cloud(NH)
+
+njw <- words_cloud(NJ)
+
+nyw <- words_cloud(NY)
+
+paw <- words_cloud(PA)
+
+riw <- words_cloud(RI)
+
+scw <- words_cloud(SC)
+
+usw <- words_cloud(US)
+
+vaw <- words_cloud(VA)
+
+vtw <- words_cloud(VT)
+
+## Displaying Word Clouds
+ctw
+dew
+gaw
+maw
+mdw
+ncw
+nhw
+njw
+nyw
+paw
+riw
+scw
+usw 
+vaw
+vtw
