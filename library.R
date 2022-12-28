67fa76f17786c5b93afdb07f3caa3850000bccb3
Benjamin Reese <108361147+BenjaminFReese@users.noreply.github.com>
2022-12-28 15:03:43 -0500

    creating qaurto doc and updating packages

--- library.R
+++ library.R
@@ -18,11 +18,12 @@ library(FactoMineR)
 library(tidymodels)
 library(topicmodels)
 library(patchwork)
+library(ldatuning)
 
 ## List of Packages
 list.of.packages <- c("tidyverse", "tm", "dbscan", "proxy", "colorspace", "tidytext", "SnowballC",
                       "igraph", "ggraph", "rvest", "wordcloud", "wordcloud2", "stringr", "RColorBrewer",
-                      "syuzhet", "FactoMineR", "tidymodels", "topicmodels", "patchwork")
+                      "syuzhet", "FactoMineR", "tidymodels", "topicmodels", "patchwork", "ldatuning")
 ## Loading Packages
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])] 
 
---
+++ sent_anal_plots.R
@@ -0,0 +1,33 @@
+## Sentiment Analysis Plots
+ct <- sent_anal(CT, "CT")
+
+de <- sent_anal(DE, "DE")
+
+ga <- sent_anal(GA, "GA")
+
+ma <- sent_anal(MA, "MA")
+
+md <- sent_anal(MD, "MD")
+
+nc <- sent_anal(NC, "NC")
+
+nj <- sent_anal(NJ, "NJ")
+
+ny <- sent_anal(NY, "NY")
+
+pa <- sent_anal(PA, "PA")
+
+ri <- sent_anal(RI, "RI")
+
+sc <- sent_anal(SC, "SC")
+
+us <- sent_anal(US, "US")
+
+va <- sent_anal(VA, "VA")
+
+vt <- sent_anal(VT, "VT")
+
+ct + de + ga + ma
+md + nc + nj + ny
+pa + ri + sc + us 
+va + vt
