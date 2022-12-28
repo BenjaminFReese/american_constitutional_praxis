761d0edd3ec48ffcb194eece40294654420f3611
Benjamin Reese <108361147+BenjaminFReese@users.noreply.github.com>
2022-12-28 14:37:03 -0500

    updating package list

--- library.R
+++ library.R
@@ -17,11 +17,12 @@ library(colorspace)
 library(FactoMineR)
 library(tidymodels)
 library(topicmodels)
+library(patchwork)
 
 ## List of Packages
 list.of.packages <- c("tidyverse", "tm", "dbscan", "proxy", "colorspace", "tidytext", "SnowballC",
                       "igraph", "ggraph", "rvest", "wordcloud", "wordcloud2", "stringr", "RColorBrewer",
-                      "syuzhet", "FactoMineR", "tidymodels", "topicmodels")
+                      "syuzhet", "FactoMineR", "tidymodels", "topicmodels", "patchwork")
 ## Loading Packages
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])] 
 
